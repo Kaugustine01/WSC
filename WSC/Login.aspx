8e3c7660447ea17c3278252fa4cf9c0532a9541c
NoThought2Ut <no.thought2it@gmail.com>
2016-03-20 18:11:34 -0700

    Updated Default Page, Re-Created Login Page.

---
+++ WSC/Login.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="WSC.Login" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <div class="content">
+        <center>
+        <asp:Login ID="Login1" runat="server" OnAuthenticate="OnAuthenticate" BackColor="#F7F7DE" BorderColor="#CCCC99"
+            BorderStyle="Solid" BorderWidth="1px" Font-Names="Verdana" Font-Size="10pt">
+            <TitleTextStyle BackColor="#6B696B" Font-Bold="True" ForeColor="#FFFFFF" />
+        </asp:Login>
+            </center>
+    </div>
+
+</asp:Content>
---
+++ WSC/Login.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WSC
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private bool SiteSpecificationAuthenticationMethod(string UserName, string Password)
+        {
+            return false;
+        }
+
+        protected void OnAuthenticate(object sender, AuthenticateEventArgs e)
+        {
+            bool Authenticated = false;
+            Authenticated = SiteSpecificationAuthenticationMethod(Login1.UserName, Login1.Password);
+
+            e.Authenticated = Authenticated;
+        }
+    }
+}
