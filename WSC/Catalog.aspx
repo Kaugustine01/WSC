b39d3860d0bf8966b08b319ce5925e6516742834
NoThought2Ut <no.thought2it@gmail.com>
2016-03-26 13:08:03 -0700

    Updated Catalog Form

--- WSC/Catalog.aspx
+++ WSC/Catalog.aspx
@@ -2,4 +2,14 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="content">
+        <center>
+            <h1>
+               WSC Catalog
+            </h1>
+            <asp:GridView ID="CatalogGridView" AutoGenerateColumns="true" runat="server">
+
+            </asp:GridView>
+        </center>
+    </div>
 </asp:Content>
--- WSC/Catalog.aspx.cs
+++ WSC/Catalog.aspx.cs
@@ -4,14 +4,24 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using BAL;
 
 namespace WSC
 {
     public partial class Catalog : System.Web.UI.Page
     {
+        BusinessLayer objBAL = new BusinessLayer();
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
+            List<CatalogItem> lCatItems = null;
+
+            //Retrieve Catalog Items
+            lCatItems = objBAL.GetCatalogItems();
+
+            CatalogGridView.DataSource = lCatItems;
+            CatalogGridView.DataBind();
         }
     }
 }
