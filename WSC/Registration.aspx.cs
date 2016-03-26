8610452469b75e7516988ae464c45dbb21e1e4d0
NoThought2Ut <no.thought2it@gmail.com>
2016-03-26 12:57:43 -0700

    Added Registration Pages.

--- WSC/Registration.aspx.cs
+++ WSC/Registration.aspx.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using BAL;
 
 namespace WSC
 {
     public partial class Registration : System.Web.UI.Page
     {
+        BusinessLayer objBAL = new BusinessLayer();
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -16,7 +19,26 @@ namespace WSC
 
         protected void btnSubmit_Click(object sender, EventArgs e)
         {
-            Boolean errors = false;
+            Customer objCustomer = null;
+
+            try
+            {
+
+                UserAccount objUA = new UserAccount(0, txtUserName.Text, txtPassword.Text, UserAccount.UserRole.Customer);
+                objUA = objBAL.InsertUser(objUA);
+
+                //Insert New Customer, will check if already exists
+                //If already exists will throw exception
+                objCustomer = new Customer(0, objUA.UserId, txtFirstName.Text, txtLastName.Text, txtAddress.Text, "", txtCity.Text, txtState.Text, txtZipCode.Text, txtPhone.Text);
+                objCustomer = objBAL.InsertCustomer(objCustomer);
+
+                Response.Redirect("RegistrationComplete.asxp");
+
+            }
+            catch (Exception)
+            {
+                Response.Redirect("RegistrationFailed.asxp");
+            }
 
         }
     }
---
+++ WSC/RegistrationComplete.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeBehind="RegistrationComplete.aspx.cs" Inherits="WSC.RegistrationComplete" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="content">
+        <center>
+            <h1>Registration Complete</h1>
+            <p>
+                Registration has been complete, please login using your User Name and Password.
+            </p>
+        </center>
+    </div>
+</asp:Content>
