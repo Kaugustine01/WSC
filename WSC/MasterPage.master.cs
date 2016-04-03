60e35fa03c1cc10bb7cc3342acf578637f912d8f
NoThought2Ut <no.thought2it@gmail.com>
2016-04-02 18:27:15 -0700

    Master Page Updated

--- WSC/MasterPage.master.cs
+++ WSC/MasterPage.master.cs
@@ -11,4 +11,44 @@ public partial class MasterPage : System.Web.UI.MasterPage
     {
         
     }
+
+    protected void btnHome_Click(Object sender, EventArgs e)
+    {
+        Response.Redirect("~/Default.aspx");
+    }
+
+    protected void btnCatalog_Click(Object sender, EventArgs e)
+    {
+        Response.Redirect("~/Catalog.aspx");
+    }
+
+    protected void btnViewCart_Click(Object sender, EventArgs e)
+    {
+        Response.Redirect("~/Catalog.aspx");
+    }
+
+    protected void btnManageAccount_Click(Object sender, EventArgs e)
+    {
+        Response.Redirect("~/ManageAccount.aspx");
+    }
+
+    protected void btnViewOrders_Click(Object sender, EventArgs e)
+    {
+        Response.Redirect("~/CustViewOrders.aspx");
+    }
+
+    protected void btnAdmViewOrders_Click(Object sender, EventArgs e)
+    {
+        Response.Redirect("~/Admin/ViewOrders.aspx");
+    }
+
+    protected void btnAddEmployees_Click(Object sender, EventArgs e)
+    {
+        Response.Redirect("~/Admin/AddEmployees.aspx");
+    }
+
+    protected void btnManageEmployees_Click(Object sender, EventArgs e)
+    {
+        Response.Redirect("~/Admin/ManageEmployees.aspx");
+    }
 }
