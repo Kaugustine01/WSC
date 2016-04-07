da014527b42075a8380e46aca03bc5990493edf6
kennethpaugustine@gmail.com <p23Xzzb7jBcn>
2016-04-07 14:06:01 -0400

    Added Email.cs to the project

---
+++ BAL/classes/Email.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Configuration;
+
+namespace BAL
+{
+    public class Email
+    {
+        SmtpClient objClient = new SmtpClient("smtp.gmail.com", 587);
+        MailAddress objFrom = new MailAddress("wscecom@gmail.com", "WSC <No Reply>");
+
+        public string Subject { get; set; }
+
+        public string Body { get; set; }
+
+        public string To { get; set; }
+
+        public Email() { }
+
+        public void SendIt()
+        {
+            //Enable SSL
+            objClient.EnableSsl = true;            
+
+            //Address TO
+            MailAddress objTo = new MailAddress(To);
+
+            //New Mail Msg
+            MailMessage objMessage = new MailMessage(objFrom, objTo);
+
+            //Body of Email
+            objMessage.Body = Body;
+
+            //Subject of Email
+            objMessage.Subject = Subject;
+
+            //Gmail UserName
+            string sUsername = ConfigurationManager.AppSettings["GmailUsername"].ToString();
+            
+            //Gmail Password
+            string sPassword = ConfigurationManager.AppSettings["GmailPassword"].ToString();
+
+            //Email Acct Credentials
+            NetworkCredential objNetworkCredential = new NetworkCredential(sUsername, sPassword);
+
+            //Set SMTP Credentials
+            objClient.Credentials = objNetworkCredential;
+
+            try
+            {
+                //Send Email
+                objClient.Send(objMessage);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+    }
+}
